4425023bbb326ba396402924a2469f3eb442b082
elsondsa <36267884+elsondsa@users.noreply.github.com>
2020-02-14 14:58:29 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,52 +1,89 @@
 package com.web;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-
-public class EmployeeRepository {
+public class Employee{
 	
-	public Connection getConnection(){
-		try{
-			Class.forName("com.mysql.jdbc.Driver");
-//			2. Get the connnection
-			return DriverManager.getConnection("jdbc:mysql://localhost:3306/DEMO?useSSL=false", "root", "root");
-			
-		}
-		catch(Exception e){
-			e.printStackTrace();
-		}
-		return null;
+	Integer id;
+	String name;
+	public Double salary;
+	
+	
+	
+	public Integer getId() {
+		return id;
 	}
-
-
-	public List<Employee> fetchAllEmployees() {
-		Connection con = getConnection();
-		
-		try{
-			Statement stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery("select * from employee");
-			ArrayList<Employee> employees = new ArrayList<>();
-			
-//			4. Get result
-			while (rs.next()){
-//				System.out.println(rs.getInt(1) + "  " + rs.getString(2));
-				Employee e = new Employee(rs.getInt("id"), rs.getString("name"), 0.0d);
-				employees.add(e);
-			}
-				
-//			5. Close DB connection
-			con.close();
-			return employees;
-		}
-		catch(Exception e){
-			e.printStackTrace();
-		}
-		return null;
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getSalary() {
+		return salary;
+	}
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	public Employee(){
 		
 	}
+	public Employee(int id, String name, double salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+	
+
+
+
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+//	@Override
+//	public int compareTo(Employee o) {
+//		return this.name.compareTo(o.name);
+//	}
+//	
+	
+//	
+//	@Override
+//	 public boolean equals(Object obj) {
+//	     Employee emp = (Employee) obj;
+//	     return this.id.equals(emp.id);
+//	 }
+	
+	
+	
+	
 
 }
